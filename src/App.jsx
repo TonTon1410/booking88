298eee25eecdabd4e9f1133d7ee5ad84ffc2a494
nhattruowng <ltn04098@gmail.com>
2024-05-22 04:16:56 +0000

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Tabbar from './tabbar/tabbar.jsx';
  import Login from './login/login.jsx';
 import MainScreen from './mainscreen/MainScreen.jsx';
+import Signup from './signup/Signup.jsx';
+
 
 function App() {
   return (
     <>
        <MainScreen/>
+       <Signup/>
     </>
   );
 }
