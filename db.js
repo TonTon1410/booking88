1f389a8a82f82477077a71fcb401a0dcae9bb081
iehuhamp <152163327+iehuhamp@users.noreply.github.com>
2024-05-23 08:19:10 +0700

    REGISTRATION API

---
+++ db.js
@@ -0,0 +1,12 @@
+{
+  "user": [
+    {
+      "id": "hiếu",
+      "name": "phạm trung hiếu",
+      "password": "123",
+      "email": "hieuphamtrung2910@gmail.com",
+      "Phone": "0967062938",
+      "address": ""
+    }
+  ]
+}
