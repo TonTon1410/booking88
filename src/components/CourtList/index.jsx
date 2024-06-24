a77c938729e07d86d12e3d1967e77fee8c9b52be
iehuhamp <152163327+iehuhamp@users.noreply.github.com>
2024-06-24 14:15:36 +0700

    add chat-box

--- src/components/CourtList/index.jsx
+++ src/components/CourtList/index.jsx
@@ -1,64 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Input, Select, Row, Col, Pagination } from 'antd';
 import CourtCard from "../CourtCard";
 import "../../components/CourtList/CourtList1.css";
 import 'bootstrap/dist/css/bootstrap.css';
-import img1 from "../../assets/img1.png";
+import api from "../../config/axios"
 
 const { Option } = Select;
 const { Search } = Input;
 
 const CourtList = () => {
-  const courtData = [
-    {
-      id: 1,
-      name: "Sân Cầu Lông Thuận Bình12",
-      location: "Quận Thủ Đức ",
-      courts: 4,
-      rating: 4,
-      image: img1,
-      operatingHours: { start: "10:00 AM", end: "10:00 PM" },
-      availableTimes: [
-        { time: "14:00 - 15:30", status: true },
-        { time: "15:30 - 17:00", status: false },
-        { time: "17:00 - 18:30", status: true },
-        { time: "18:30 - 20:00", status: true },
-        { time: "20:00 - 21:30", status: true },
-      ],
-    },{
-      id: 1,
-      name: "Sân Cầu Lông A",
-      location: "Hồ Chí Minh ",
-      courts: 4,
-      rating: 4,
-      image: img1,
-      operatingHours: { start: "10:00 AM", end: "10:00 PM" },
-      availableTimes: [
-        { time: "14:00 - 15:30", status: true },
-        { time: "15:30 - 17:00", status: false },
-        { time: "17:00 - 18:30", status: true },
-        { time: "18:30 - 20:00", status: true },
-        { time: "20:00 - 21:30", status: true },
-      ],
-    },{
-      id: 1,
-      name: "Sân Cầu Lông B",
-      location: "Hà Nội  ",
-      courts: 4,
-      rating: 4,
-      image: img1,
-      operatingHours: { start: "10:00 AM", end: "10:00 PM" },
-      availableTimes: [
-        { time: "14:00 - 15:30", status: true },
-        { time: "15:30 - 17:00", status: false },
-        { time: "17:00 - 18:30", status: true },
-        { time: "18:30 - 20:00", status: true },
-        { time: "20:00 - 21:30", status: true },
-      ],
-    },
-    // Add other courts here
-  ];
-
+  const [courtData, setCourtData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedTime, setSelectedTime] = useState("");
@@ -66,6 +18,22 @@ const CourtList = () => {
   const [selectedOperatingHour, setSelectedOperatingHour] = useState("");
 
   const courtsPerPage = 6;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/get-all-club")
+        if (Array.isArray(response.data)) {
+          setCourtData(response.data);
+        } else {
+          console.error("API response is not an array:", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching court data:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleTimeChange = (value) => {
     setSelectedTime(value);
@@ -79,26 +47,28 @@ const CourtList = () => {
     setSelectedOperatingHour(value);
   };
 
-  const filteredCourts = courtData
-    .filter(
-      (court) =>
-        court.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        court.location.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-    .filter((court) => {
-      if (selectedOperatingHour === "") return true;
-      return court.operatingHours.start === selectedOperatingHour;
-    })
-    .filter((court) => {
-      if (selectedLocation === "") return true;
-      return court.location.includes(selectedLocation);
-    })
-    .filter((court) => {
-      if (selectedTime === "") return true;
-      return court.availableTimes.some(
-        (timeSlot) => timeSlot.time.startsWith(selectedTime) && timeSlot.status
-      );
-    });
+  const filteredCourts = Array.isArray(courtData)
+    ? courtData
+        .filter(
+          (court) =>
+            court.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            court.location.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .filter((court) => {
+          if (selectedOperatingHour === "") return true;
+          return court.operatingHours.start === selectedOperatingHour;
+        })
+        .filter((court) => {
+          if (selectedLocation === "") return true;
+          return court.location.includes(selectedLocation);
+        })
+        .filter((court) => {
+          if (selectedTime === "") return true;
+          return court.availableTimes.some(
+            (timeSlot) => timeSlot.time.startsWith(selectedTime) && timeSlot.status
+          );
+        })
+    : [];
 
   const indexOfLastCourt = currentPage * courtsPerPage;
   const indexOfFirstCourt = indexOfLastCourt - courtsPerPage;
