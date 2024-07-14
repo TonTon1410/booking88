e61bd6448d4a2000949fd92bd3dd3ddcd6671bbd
iehuhamp <152163327+iehuhamp@users.noreply.github.com>
2024-07-15 00:00:35 +0700

    update booking fixed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -116,13 +116,6 @@ const Login = () => {
               <GGLogin />
             </GoogleOAuthProvider>
           </div>
-
-
-          <div className="field button-field">
-            <GoogleOAuthProvider clientId={clientId}>
-              <GGLogin />
-            </GoogleOAuthProvider>
-          </div>
         </div>
       </div>
     </div>
