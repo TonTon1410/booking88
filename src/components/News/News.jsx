e72109fce0a1df3564a36a7d6dfafb4a42a281af
ToanSE172763 <Toanpvse172763@fpt.edu.vn>
2024-07-20 02:10:27 +0700

    update dashboard

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import './News.scss';
+import { useEffect, useState } from "react";
+import "./News.scss";
 
 const News = () => {
   const [articles, setArticles] = useState([]);
@@ -13,22 +13,25 @@ const News = () => {
   const fetchNews = async () => {
     try {
       const response = await fetch(
-        'https://newsapi.org/v2/everything?q=badminton&apiKey=cf6398c529664dff8b76be5e5e895293'
+        "https://newsapi.org/v2/everything?q=badminton&apiKey=cf6398c529664dff8b76be5e5e895293"
       );
       const data = await response.json();
       setArticles(data.articles);
     } catch (error) {
-      console.error('Error fetching news:', error);
+      console.error("Error fetching news:", error);
     }
   };
 
   const indexOfLastArticle = currentPage * articlesPerPage;
   const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
-  const currentArticles = articles.slice(indexOfFirstArticle, indexOfLastArticle);
+  const currentArticles = articles.slice(
+    indexOfFirstArticle,
+    indexOfLastArticle
+  );
 
-  const paginate = pageNumber => {
+  const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
-    window.scrollTo(0, 0);  // Cuộn lên đầu trang
+    window.scrollTo(0, 0); // Cuộn lên đầu trang
   };
 
   return (
@@ -36,25 +39,34 @@ const News = () => {
       {currentArticles.map((article, index) => (
         <div key={index} className="news-item">
           {article.urlToImage && (
-            <img src={article.urlToImage} alt={article.title} className="news-image" />
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+              className="news-image"
+            />
           )}
           <div className="news-content">
             <h2>{article.title}</h2>
             <p>{article.description}</p>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">Đọc thêm</a>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+              Đọc thêm
+            </a>
           </div>
         </div>
       ))}
       <div className="pagination">
-        {Array.from({ length: Math.ceil(articles.length / articlesPerPage) }, (_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => paginate(index + 1)}
-            className={currentPage === index + 1 ? 'active' : ''}
-          >
-            {index + 1}
-          </button>
-        ))}
+        {Array.from(
+          { length: Math.ceil(articles.length / articlesPerPage) },
+          (_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => paginate(index + 1)}
+              className={currentPage === index + 1 ? "active" : ""}
+            >
+              {index + 1}
+            </button>
+          )
+        )}
       </div>
     </div>
   );
--- src/components/promotion/index.jsx
+++ src/components/promotion/index.jsx
@@ -1,6 +1,7 @@
-import { Button, Form, Input, InputNumber, Modal, Select, Table, Tag } from "antd";
+import { Button, Form, Input, DatePicker, Modal, Table, Tag, InputNumber, Popconfirm } from "antd";
 import { useForm } from "antd/es/form/Form";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import moment from "moment";
 import api from "../../config/axios";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../redux/features/counterSlice";
@@ -10,6 +11,7 @@ function Promotion() {
   const [form] = useForm();
   const [data, setData] = useState([]);
   const user = useSelector(selectUser);
+
   const fetchDataPromotion = async () => {
     try {
       const response = await api.get(`/promotion/${user.idLocation}`);
@@ -19,83 +21,118 @@ function Promotion() {
       console.log(e);
     }
   };
+
   const onFinish = async (value) => {
-    console.log(value)
-    if (value.id == undefined) {
+    const startDate = value.startDate;
+    const endDate = value.endDate;
+
+    // Check if start date is after end date
+    if (startDate.isAfter(endDate)) {
+      form.setFields([
+        {
+          name: 'startDate',
+          errors: ['Ngày bắt đầu phải nhỏ hơn ngày kết thúc!'],
+        },
+        {
+          name: 'endDate',
+          errors: ['Ngày kết thúc phải lớn hơn ngày bắt đầu!'],
+        },
+      ]);
+      return;
+    }
+
+    const formattedValue = {
+      ...value,
+      startDate: startDate.format("DD/MM/YYYY"),
+      endDate: endDate.format("DD/MM/YYYY"),
+      status: "ACTIVE",
+    };
+
+    if (value.id === undefined) {
       try {
-        const response = await api.post(`/promotion`, {
-          "discount": 0,
-          "startDate": value.startDate,
-          "endDate": value.endDate,
-          "status": "ACTIVE",
-          "code": value.code,
-        });
+        const response = await api.post(`/promotion`, formattedValue);
         console.log(response.data);
         setData([...data, response.data]);
-        setShowModal(false)
+        setShowModal(false);
       } catch (e) {
         console.log(e);
       }
     } else {
       try {
-        const res = await api.put(`/promotion/${value.id}`, value)
-        console.log("res: ", res.data)
-        setData((oldData) => (
+        const res = await api.put(`/promotion/${value.id}`, formattedValue);
+        console.log("res: ", res.data);
+        setData((oldData) =>
           oldData.map((oldItem) => {
-            if (oldItem.id == value.id) {
+            if (oldItem.id === value.id) {
               return res.data;
             } else {
               return oldItem;
             }
           })
-        )
-
-
         );
-        setShowModal(false)
+        setShowModal(false);
         console.log("data: ", data);
       } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
       }
-      form.resetFields()
+      form.resetFields();
     }
   };
-  const handleDelete = async (value) => {
-    console.log("Delete: ", value)
-    try {
-      const res = await api.delete(`/promotion/${value.id}`)
-      console.log(res.data)
-      setData(data.filter(data => data.id != res.data))
-    } catch (error) {
-      console.log(error.message)
-    }
 
-  }
-  const handleUpdate = async (value) => {
-    form.setFieldsValue(value)
-    setShowModal(true)
+  const handleDelete = async (value) => {
+    console.log("Delete: ", value);
+    try {
+      const res = await api.delete(`/promotion/${value.id}`);
+      console.log(res.data);
+      setData(data.filter((data) => data.id !== value.id));
+    } catch (error) {
+      console.log(error.message);
+    }
   };
+
+  const handleUpdate = async (value) => {
+    form.setFieldsValue({
+      ...value,
+      startDate: moment(value.startDate, "DD/MM/YYYY"),
+      endDate: moment(value.endDate, "DD/MM/YYYY"),
+    });
+    setShowModal(true);
+  };
+
   useEffect(() => {
     fetchDataPromotion();
   }, []);
+
+  const disabledStartDate = (current) => {
+    return current && current < moment().startOf('day');
+  };
+
+  const disabledEndDate = (current) => {
+    const startDate = form.getFieldValue('startDate');
+    if (!startDate) {
+      return current && current < moment().startOf('day');
+    }
+    return current && (current < moment().startOf('day') || current <= moment(startDate).startOf('day'));
+  };
+
   const columns = [
     {
-      title: "Name",
+      title: "Code",
       dataIndex: "code",
       key: "code",
     },
     {
-      title: "Discount",
+      title: "Giảm giá(%)",
       dataIndex: "discount",
       key: "discount",
     },
     {
-      title: "startDate",
+      title: "Ngày bắt đầu",
       dataIndex: "startDate",
       key: "startDate",
     },
     {
-      title: "endDate",
+      title: "Ngày kết thúc",
       dataIndex: "endDate",
       key: "endDate",
     },
@@ -103,9 +140,7 @@ function Promotion() {
       title: "Status",
       dataIndex: "status",
       key: "status",
-      render: (e) => (
-        <Tag color={e == "ACTIVE" ? "green" : "red"}>{e}</Tag>
-      )
+      render: (e) => <Tag color={e === "ACTIVE" ? "green" : "red"}>{e}</Tag>,
     },
     {
       title: "Action",
@@ -113,16 +148,24 @@ function Promotion() {
       key: "address",
       render: (_, value) => (
         <>
-          <Button danger onClick={() => (handleUpdate(value))
-          }>Update</Button>
-          <Button
-            style={{
-              marginLeft: "20px",
-            }}
-            onClick={() => handleDelete(value)}
-          >
-            Delete
+          <Button danger onClick={() => handleUpdate(value)}>
+            Sửa
           </Button>
+          <Popconfirm
+            title="Bạn có chắc chắn muốn xóa mã giảm giá này không?"
+            onConfirm={() => handleDelete(value)}
+            okText="Có"
+            cancelText="Không"
+          >
+            <Button
+              style={{
+                marginLeft: "20px",
+                
+              }}
+            >
+              Xóa
+            </Button>
+          </Popconfirm>
         </>
       ),
     },
@@ -136,6 +179,7 @@ function Promotion() {
         onOk={() => form.submit()}
         onCancel={() => setShowModal(false)}
         open={showModal}
+        afterClose={() => form.resetFields()}
       >
         <Form
           form={form}
@@ -149,67 +193,50 @@ function Promotion() {
           }}
           autoComplete="off"
         >
-          <Form.Item name="id"></Form.Item>
+          <Form.Item name="id" hidden></Form.Item>
           <Form.Item
             label="Code"
             name="code"
             rules={[
               {
                 required: true,
-                message: "Please input your Code!",
+                message: "Hãy nhập tên của mã giảm giá!",
               },
             ]}
           >
             <Input />
           </Form.Item>
-          <Form.Item label="Status" name="status" rules={[
-            {
-              required: true,
-              message: "Please input your Status!",
-            },
-          ]}>
-            <Select options={[
-              {
-                value: "ACTIVE",
-                label: "ACTIVE"
-              },
-              {
-                value: "INACTIVE",
-                label: "INACTIVE"
-              },
-            ]} />
-          </Form.Item>
           <Form.Item
-            label="Giờ mở cửa"
+            label="Ngày bắt đầu"
             name="startDate"
             rules={[
               {
                 required: true,
-                message: "Please input your startDate!",
+                message: "Hãy nhập ngày bắt đầu!",
               },
             ]}
           >
-            <InputNumber addonAfter="Giờ" />
+            <DatePicker format="DD/MM/YYYY" disabledDate={disabledStartDate} />
           </Form.Item>
           <Form.Item
-            label="Giờ đóng cửa"
+            label="Ngày kết thúc"
             name="endDate"
             rules={[
               {
                 required: true,
-                message: "Please input your endDate!",
+                message: "Hãy nhập ngày kết thúc!",
               },
             ]}
           >
-            <InputNumber addonAfter="Giờ" />
+            <DatePicker format="DD/MM/YYYY" disabledDate={disabledEndDate} />
           </Form.Item>
           <Form.Item
-            label="Discount"
+            label="Giảm giá(%)"
             name="discount"
             rules={[
               {
                 required: true,
-                message: "Please input your discount!",
+                message: "Hãy nhập số % giảm giá!",
               },
             ]}
           >
